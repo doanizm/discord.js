28674cb30892e1ce1926ee3cfb4df63a22a40a7e
Doğan <81542259+doanizm@users.noreply.github.com>
2021-04-30 17:58:34 +0300

    Update Collection.js

--- src/util/Collection.js
+++ src/util/Collection.js
@@ -4,8 +4,7 @@ const BaseCollection = require('@discordjs/collection');
 const Util = require('./Util');
 
 class Collection extends BaseCollection {
-  toJSON() {
-    return this.map(e => (typeof e?.toJSON === 'function' ? e.toJSON() : Util.flatten(e)));
+    return this.map(e => (typeof e? toJSON === 'function' ? e.toJSON() : Util.flatten(e)));
   }
 }
 
